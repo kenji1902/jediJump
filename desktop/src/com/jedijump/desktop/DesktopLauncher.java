76765cb5c0626d4d9fc92f71f7358a7537a29b2f
Kenji Shimizu <kenjishimizu1717@gmail.com>
2022-03-07 20:54:42 +0800

    Initial push

---
+++ desktop/src/com/jedijump/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jedijump.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jedijump.jediJump;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new jediJump(), config);
+	}
+}
