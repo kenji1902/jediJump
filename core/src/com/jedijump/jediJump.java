0a2ae37cdaa553b59484d1764e6cd59f1e095747
Kenji Shimizu <kenjishimizu1717@gmail.com>
2022-03-08 16:04:04 +0800

    realigned to PlayState

--- core/src/com/jedijump/jediJump.java
+++ core/src/com/jedijump/jediJump.java
@@ -14,55 +14,31 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.jedijump.entity.character;
 import com.jedijump.entity.platform;
 import com.jedijump.states.Manager;
+import com.jedijump.states.PlayState;
 import com.jedijump.utility.constants;
 
 public class jediJump extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	character character;
-	platform plt;
 	Manager manage;
-
-
-	private Box2DDebugRenderer b2dr;
 
 	@Override
 	public void create () {
-
-		b2dr = new Box2DDebugRenderer();
 		batch = new SpriteBatch();
-
-
 		manage = new Manager();
-
-		character = new character(manage);
-		plt = new platform(manage);
-
-		character.create(new Vector2(0,0),new Vector2(32,32),1);
-		plt.create(new Vector2(0,-36),new Vector2(80,32),0);
+		manage.push(new PlayState(manage));
 	}
 
 	@Override
 	public void render () {
-
 		ScreenUtils.clear(0, 0, 0, 1);
-		b2dr.render(manage.getWorld(),manage.getCamera().combined.scl(constants.PPM));
-
 		float delta = Gdx.graphics.getDeltaTime();
-		manage.getWorld().step(1/60f,6,2);
-
-		character.update(delta);
-		character.render(batch);
-
-		plt.update(delta);
-		plt.render(batch);
-
+		manage.update(delta);
+		manage.render(batch);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
-		b2dr.dispose();
+		manage.disposeAll();
 	}
 }
--- core/src/com/jedijump/states/PlayState.java
+++ core/src/com/jedijump/states/PlayState.java
@@ -1,20 +1,36 @@
 package com.jedijump.states;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.jedijump.entity.character;
+import com.jedijump.entity.platform;
 
 public class PlayState extends State{
-
-    protected PlayState(Manager manager) {
+    character character;
+    platform plt;
+    public PlayState(Manager manager) {
         super(manager);
+        character = new character(manager);
+        plt = new platform(manager);
+
+        character.create(new Vector2(0,0),new Vector2(32,32),1);
+        plt.create(new Vector2(0,-36),new Vector2(80,32),0);
     }
 
     @Override
     public void update(float delta) {
-
+        character.update(delta);
+        plt.update(delta);
     }
 
     @Override
     public void render(SpriteBatch sprite) {
+        character.render(sprite);
+        plt.render(sprite);
+    }
+
+    @Override
+    public void dispose() {
 
     }
 }
