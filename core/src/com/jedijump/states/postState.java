877cfe62a2603cb23e65be8058ea50d2996b27d0
Francis Miguel Dela Cruz <hyper15francismiguel@gmail.com>
2022-03-09 22:13:18 +0800

    Update postState

--- core/src/com/jedijump/states/postState.java
+++ core/src/com/jedijump/states/postState.java
@@ -11,8 +11,9 @@ import com.badlogic.gdx.math.Vector3;
 import com.jedijump.utility.constants;
 
 public class postState extends State {
-    static final int GAME_RETRY = 0;
-    static final int GAME_QUIT = 1;
+    static final int GAME_GAMEOVER = 0;
+    static final int GAME_RETRY = 1;
+    static final int GAME_QUIT = 2;
     private int state;
     Texture item, bg;
     TextureRegion retry, quit, bgRegion;
@@ -25,8 +26,8 @@ public class postState extends State {
         super(manager);
 
         item = new Texture(Gdx.files.internal("items.png"));
-        retry = new TextureRegion(item, 151, 30, 348, 353);
-        quit = new TextureRegion(item, 128, 31, 363, 386);
+        retry = new TextureRegion(item, 348, 353, 151, 30);
+        quit = new TextureRegion(item, 363, 386, 128, 31);
         rect = new Rectangle(55, 150 , 64, 64);
         retry_rect = new Rectangle(65, 250, 192,96/2);
         quit_rect = new Rectangle(65, 200, 192,96/2);
@@ -43,6 +44,9 @@ public class postState extends State {
     @Override
     public void update(float delta) {
         switch (state) {
+            case GAME_GAMEOVER:
+                break;
+
             case GAME_RETRY:
                 manager.set(new PlayState(manager));
                 break;
@@ -102,7 +106,7 @@ public class postState extends State {
         rect.y = 150 + camera.position.y;
         batch.begin();
         batch.draw(retry,  rect.x, rect.y, rect.width, rect.height);
-        batch.draw(quit, rect.y, rect.y, rect.width, rect.height);
+        batch.draw(quit, rect.x, rect.y, rect.width, rect.height);
         batch.end();
 
         if(state == 0){
@@ -110,26 +114,9 @@ public class postState extends State {
 
             batch.begin();
             batch.draw(bgRegion, 0,0,constants.SCREENWIDTH, constants.SCREENHEIGHT);
-            batch.draw(retry, retry_rect.x, retry_rect.y-50);
+            batch.draw(retry, retry_rect.x + 25, retry_rect.y-10);
+            batch.draw(quit, quit_rect.x + 35, quit_rect.y - 5);
 
-            batch.end();
-//            sr.setProjectionMatrix(manager.getCamera().combined);
-//            sr.begin(ShapeRenderer.ShapeType.Filled);
-//            sr.rect(resume_rect.x, resume_rect.y, resume_rect.width, resume_rect.height);
-//            sr.setColor(Color.GREEN);
-//            sr.end();
-//            sr2.setProjectionMatrix(manager.getCamera().combined);
-//            sr2.begin(ShapeRenderer.ShapeType.Filled);
-//            sr2.rect(quit_rect.x, quit_rect.y, quit_rect.width, quit_rect.height);
-//            sr2.setColor(Color.RED);
-//            sr2.end();
-        }
-        if(state == 1){
-            camera.setToOrtho(false);
-
-            batch.begin();
-            batch.draw(bgRegion, 0,0, constants.SCREENWIDTH, constants.SCREENHEIGHT);
-            batch.draw(quit, quit_rect.x, quit_rect.y-50);
             batch.end();
         }
     }
