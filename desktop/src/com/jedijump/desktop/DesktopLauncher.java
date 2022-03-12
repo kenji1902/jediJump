95ab17f1e6e10601e5f496bb02e898e9a13fbe10
Kenji Shimizu <kenjishimizu1717@gmail.com>
2022-03-12 14:29:54 +0800

    Database

--- desktop/src/com/jedijump/desktop/DesktopLauncher.java
+++ desktop/src/com/jedijump/desktop/DesktopLauncher.java
@@ -4,6 +4,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.jedijump.jediJump;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
@@ -12,5 +16,7 @@ public class DesktopLauncher {
 		config.foregroundFPS = 60;
 		config.backgroundFPS = 60;
 		new LwjglApplication(new jediJump(), config);
+
+
 	}
 }
